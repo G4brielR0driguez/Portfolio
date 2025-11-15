4ebb96091e7ea803c35e67110ad22a28918f46ff
Gabriel <rgabriellopez2@gmail.com>
2025-11-15 20:03:04 -0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -2,17 +2,39 @@
 const openside = document.querySelector(".display-hamburguesa");
 const difuminadorback = document.getElementById ("fondo");
 const sidebar = document.querySelector(".sidebar");
+const media = window.matchMedia("(min-width: 1025px)");
+const buttonsside = document.getElementById("botones-sidebar");
+
 if(openside && difuminadorback){
  openside.onclick = () =>{
     difuminadorback.style.display = "flex";
-    sidebar.style.display = "flex";
+    sidebar.classList.add("active");
+    setTimeout(() => {
+    sidebar.scrollTo({
+      top: 0,             
+      behavior: "smooth"
+    });
+  }, 300);
+}
+
  } 
 
  window.onclick = (e) => {
     if (e.target === difuminadorback) {
       difuminadorback.style.display = "none";
-        sidebar.style.display = "none";
+     sidebar.classList.remove("active");
     }
-  };
+  }
 
+  buttonsside.onclick = () =>{
+    sidebar.classList.remove("active");
+    difuminadorback.style.display = "none";
+  }
+  function updateDisplay(e) {
+    if (e.matches) {
+        sidebar.classList.remove("active");
+        difuminadorback.style.display = "none";
 }
+}
+media.addEventListener("change", updateDisplay);
+updateDisplay(media);
